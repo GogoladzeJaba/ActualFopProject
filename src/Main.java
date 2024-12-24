8bf9a503e2739bd32837e9bb96699e6de074684b
jaba <jabagogoladzis3@gmail.com>
2024-12-24 14:30:56 +0400

    commit test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hi hi hi hi hi hello");
+        System.out.println("hi hi hi hi hi hellooooo");
     }
 }
