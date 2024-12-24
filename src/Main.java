24bfc86124bdcefb976d5c82bea187a1be37628c
jaba <jabagogoladzis3@gmail.com>
2024-12-24 16:36:09 +0400

    Testing renaming branch again

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Testing renaming branch");
+        System.out.println("Testing renaming branch again");
     }
 }
